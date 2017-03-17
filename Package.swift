18cd11f1131eec5a4778b9cac015b4c555e96359
Steve Whittle <steve.whittle@protonmail.com>
2017-03-17 22:04:47 +0000

    now compiles as a shared object

--- Package.swift
+++ Package.swift
@@ -3,13 +3,22 @@ import PackageDescription
 let package = Package(
     name: "Atomic",
     dependencies: [
-        .Package (
-            url:          "https://github.com/itssofluffy/ISFLibrary.git",
-            majorVersion: 0
-        ),
-        .Package (
-            url:          "https://github.com/itssofluffy/Mutex.git",
-            majorVersion: 0
-        )
+        .Package (url: "https://github.com/itssofluffy/ISFLibrary.git", majorVersion: 0),
+        .Package (url: "https://github.com/itssofluffy/Mutex.git", majorVersion: 0)
     ]
 )
+
+let staticLibrary = Product(
+    name:    "Atomic",
+    type:    .Library(.Static),
+    modules: ["Atomic"]
+)
+
+let dynamicLibrary = Product(
+    name:    "Atomic",
+    type:    .Library(.Dynamic),
+    modules: ["Atomic"]
+)
+
+products.append(staticLibrary)
+products.append(dynamicLibrary)

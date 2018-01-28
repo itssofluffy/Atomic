c047c294bc194d2a0e8e150f4826c100be5a89d3
itssofluffy <steve.whittle@protonmail.com>
2018-01-28 12:11:28 +0000

    compile to a static library only

--- Sources/Atomic/Core/Equatable.swift
+++ Sources/Atomic/Core/Equatable.swift
@@ -1,7 +1,7 @@
 /*
     Equatable.swift
 
-    Copyright (c) 2017 Stephen Whittle  All rights reserved.
+    Copyright (c) 2017, 2018 Stephen Whittle  All rights reserved.
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"),
